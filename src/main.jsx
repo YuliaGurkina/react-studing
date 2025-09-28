1a52f48428aa15a43705836ebac5f70d4b6ccfab
Yulia <lyulyamir@gmail.com>
2025-09-28 22:21:52 +0300

    хук useLocalStorage()

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Demo from "./Demo.jsx";
+import Demo from "./Demo.tsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
