5907537870ef5d79234cdcb3d69e10b1f7f9a7f1
Yulia <lyulyamir@gmail.com>
2025-09-29 12:15:25 +0300

    Исправления и пример другой реализации

--- src/useLocalStorage.tsx
+++ src/useLocalStorage.tsx
@@ -48,3 +48,40 @@ export const useLocalStorage: UseLocalStorage = (key: string) => {
 
   return [value, { setItem, removeItem }];
 };
+
+/*
+import { useState } from 'react';
+
+type LocalStorageSetValue = string;
+type LocalStorageReturnValue = LocalStorageSetValue | null;
+
+type UseLocalStorage = (key: string) => [
+  value: LocalStorageReturnValue,
+  {
+    setItem: (value: LocalStorageSetValue) => void;
+    removeItem: () => void;
+  },
+];
+
+export const useLocalStorage: UseLocalStorage = key => {
+  const [value, setValue] = useState<LocalStorageReturnValue>(() => localStorage.getItem(key));
+
+  function setItem(value: LocalStorageSetValue) {
+    setValue(value);
+    localStorage.setItem(key, value);
+  }
+
+  function removeItem() {
+    setValue(null);
+    localStorage.removeItem(key);
+  }
+
+  return [
+    value,
+    {
+      setItem,
+      removeItem,
+    },
+  ];
+};
+*/
